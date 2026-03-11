b01c7129789319c783f91558d62619bbe6557ad5
allan raymond <raymonda916@gmail.com>
2026-03-11 22:02:32 +0300

    added webhook route

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.js'
 import paymentRoutes from './routes/payments.js'
+import webhookRoutes from './routes/webhooks.js'
 
 dotenv.config()
 const app = express()
@@ -21,6 +22,7 @@ app.get('/ping', (req, res) => res.json({ message: 'pong' }))
 app.use(express.json())
 app.use('/auth', authRoutes)
 app.use('/payments', paymentRoutes)
+app.use('/webhooks', webhookRoutes)
 
 
 app.listen(process.env.PORT, () => { console.log(`Server running on port ${process.env.PORT}`) })
