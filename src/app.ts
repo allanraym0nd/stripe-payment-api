daf9e216b5610b244bf0264eaac9afa6b5bab186
allan raymond <raymonda916@gmail.com>
2026-03-11 11:25:54 +0300

    added payments + refund route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.js'
+import paymentRoutes from './routes/payments.js'
 
 dotenv.config()
 const app = express()
@@ -15,9 +16,11 @@ app.use(cookieParser())
 
 //for webhooks
 app.use('/webhooks/stripe', express.raw({ type: 'application/json' }))
+app.get('/ping', (req, res) => res.json({ message: 'pong' }))
 
 app.use(express.json())
 app.use('/auth', authRoutes)
+app.use('/payments', paymentRoutes)
 
 
 app.listen(process.env.PORT, () => { console.log(`Server running on port ${process.env.PORT}`) })
