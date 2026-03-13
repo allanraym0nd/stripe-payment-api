e38c0c94975ba604124e793540cb446188485d04
allan raymond <raymonda916@gmail.com>
2026-03-13 16:56:31 +0300

    added pino logger middleware

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,15 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.js'
 import paymentRoutes from './routes/payments.js'
 import webhookRoutes from './routes/webhooks.js'
+import { generalLimiter } from './middleware/rateLimiter.js'
+import { errorHandler } from './middleware/errorHandler.js'
+import { requestLogger } from './middleware/requestLogger.js'
+import logger from './utils/logger.js'
 
 dotenv.config()
 const app = express()
 
+app.use(requestLogger)
 //SECURITY
 app.use(helmet())
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
@@ -20,11 +25,15 @@ app.use('/webhooks/stripe', express.raw({ type: 'application/json' }))
 app.get('/ping', (req, res) => res.json({ message: 'pong' }))
 
 app.use(express.json())
+app.use(generalLimiter)
 app.use('/auth', authRoutes)
 app.use('/payments', paymentRoutes)
 app.use('/webhooks', webhookRoutes)
 
 
-app.listen(process.env.PORT, () => { console.log(`Server running on port ${process.env.PORT}`) })
+app.use(errorHandler)
+
+
+app.listen(process.env.PORT, () => { logger.info({ port: process.env.PORT }, 'server started') })
 
 export default app;
